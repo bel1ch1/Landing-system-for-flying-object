1382163040b8955f71213edabe0ade395a20436a
bel1c1 <andreyzv5555@gmail.com>
2024-05-01 17:24:07 +0300

    init detection file

---
+++ detection/detection.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy
+import pickle
+
+from constantes import MARKER_SIZE_M, ARUCO_PARAM
+
+# Подгружаем данные для определения дистанции
+with open('calibration_params//dist.pkl', 'rb') as f:
+    dist_coef = pickle.load(f)
+
+with open('calibration_params//cameraMatrix.pkl', 'rb') as g:
+    cam_mat = pickle.load(g)
